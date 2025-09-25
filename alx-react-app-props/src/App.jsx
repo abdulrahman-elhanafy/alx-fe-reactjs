03609f60e7471f395d48134a9aff2d3914b69d1b
Abdulrahman Hanafy <abdulrahman.elhanafy.dev@gmail.com>
2025-09-25 23:57:56 +0300

    Redirected Files

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,14 @@
-import UserContext from './UserContext';
-import ProfilePage from './ProfilePage';
+import UserContext from "./components/UserContext";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
-  );
+    return (
+        <UserContext.Provider value={userData}>
+            <ProfilePage />
+        </UserContext.Provider>
+    );
 }
 
 export default App;
