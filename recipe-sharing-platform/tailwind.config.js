29538e671fdde9a8f05ef4ba5d521b91c9d1426c
Abdulrahman Hanafy <abdulrahman.elhanafy.dev@gmail.com>
2025-10-04 22:41:08 +0300

    Set up TailWind Project

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+export default {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: false, // أو media / class
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
