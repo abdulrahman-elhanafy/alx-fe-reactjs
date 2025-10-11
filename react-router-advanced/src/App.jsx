afd6daa8cbbb93f1941a867360ab8359493c7c95
Abdulrahman Hanafy <abdulrahman.elhanafy.dev@gmail.com>
2025-10-11 16:12:17 +0300

    final

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,24 +1,30 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Profile from "./components/Profile";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import BlogPost from "./components/BlogPost"; // تأكد إن الملف ده موجود
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
 
-            {/* Protected Route for Profile */}
-            <Route
-                path="/profile"
-                element={
-                    <ProtectedRoute>
-                        <Profile />
-                    </ProtectedRoute>
-                }
-            />
-        </Routes>
+                {/* Dynamic route for blog posts */}
+                <Route path="/blog/:id" element={<BlogPost />} />
+
+                {/* Protected Route for Profile */}
+                <Route
+                    path="/profile"
+                    element={
+                        <ProtectedRoute>
+                            <Profile />
+                        </ProtectedRoute>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
     );
 }
