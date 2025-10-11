1a93bf50d9d2215c55164c603fb45255f36bec55
Abdulrahman Hanafy <abdulrahman.elhanafy.dev@gmail.com>
2025-10-11 16:03:29 +0300

    done

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 export default function Profile() {
     return (
         <div className="p-6">
             <h2>Profile Page 👤</h2>
+
             <nav className="mt-2 flex gap-3">
                 <Link to="details">Details</Link>
                 <Link to="settings">Settings</Link>
             </nav>
+
             <hr className="my-4" />
-            <Outlet />
+
+            {/* 🧩 Nested Routes داخل Profile.jsx */}
+            <Routes>
+                <Route path="details" element={<ProfileDetails />} />
+                <Route path="settings" element={<ProfileSettings />} />
+            </Routes>
         </div>
     );
 }
