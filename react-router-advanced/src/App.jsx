320aa56ae4363e7791c49ce59229290ac24e4884
Abdulrahman Hanafy <abdulrahman.elhanafy.dev@gmail.com>
2025-10-11 16:10:38 +0300

    finish

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,39 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Profile from "./components/Profile";
+import Home from "./components/Home";
 import Login from "./components/Login";
-import BlogPost from "./components/BlogPost";
-import ProtectedRoute from "./components/ProtectedRoute"; // ✅ الجديد
 
 export default function App() {
-    const [isAuthenticated, setIsAuthenticated] = React.useState(false);
-
     return (
-        <Router>
-            <Navbar
-                isAuthenticated={isAuthenticated}
-                setAuth={setIsAuthenticated}
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+
+            {/* Protected Route for Profile */}
+            <Route
+                path="/profile"
+                element={
+                    <ProtectedRoute>
+                        <Profile />
+                    </ProtectedRoute>
+                }
             />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route
-                    path="/login"
-                    element={<Login setAuth={setIsAuthenticated} />}
-                />
-
-                <Route
-                    path="/dashboard"
-                    element={
-                        <ProtectedRoute>
-                            <Dashboard />
-                        </ProtectedRoute>
-                    }
-                />
-
-                <Route path="/blog/:id" element={<BlogPost />} />
-            </Routes>
-        </Router>
+        </Routes>
     );
 }
